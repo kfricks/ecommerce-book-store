810ed9c76df2548e02def7fbea65053ec8cf2375
Katie Fricks <katiefricks@gmail.com>
2018-10-15 12:04:50 -0400

    Adjusted update method on the profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,14 @@
 class ProfileController < ApplicationController
   def show
-    current_user.name = "Katie"
-  end
-
-  def update
+    # current_user.name = "Katie"
   end
 
   def edit
+  end
+
+  def update
+    current_user.name = params[:user][:name]
+    current_user.save
+    redirect_to profile_path
   end
 end
