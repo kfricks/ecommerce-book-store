ac80ab7322958f840e912f1be53a8dc9d315f94c
Katie Fricks <katiefricks@gmail.com>
2018-10-15 11:47:13 -0400

    Created profile edit view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "profile", to: "profile#show"
-  get "profile/edit", to: "profile#edit"
-  put "profile", to: "profile#update"
+  get     "profile",      to: "profile#show"
+  put     "profile",      to: "profile#update"
+  patch   "profile",      to: "profile#update"
+  get     "profile/edit", to: "profile#edit"
 end
