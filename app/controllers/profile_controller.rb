624c28e29f55db80b6cd25073b10fd8bff40b6b3
Katie Fricks <katiefricks@gmail.com>
2018-10-15 14:32:42 -0400

    added photo upload

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,8 +7,18 @@ class ProfileController < ApplicationController
   end
 
   def update
-    current_user.name = params[:user][:name]
-    current_user.save
-    redirect_to profile_path
+    current_user.update_attributes(user_params)
+
+    if current_user.save
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
   end
 end
