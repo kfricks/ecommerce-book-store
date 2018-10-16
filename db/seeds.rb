c44b117424a9a1605655727bcfd1ed4cf485f27b
Katie Fricks <katiefricks@gmail.com>
2018-10-16 10:20:36 -0400

    Attempting to make image variants work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Amy", email: "katie@katie.com", password:"tester")
+
+kurt_v = User.create!(name: "Ron Rash", email: "kurt@kurt.com", password:"tester")
+
+books: = [
+  { title: "Book1", price: 15.99 },
+  { title: "Book2", price: 24.99 }
+  ]
+books.each do |title, price|
+    Book.create!(title: book[:title], price: book[:price])
+end
+
+kurt_v.books << Book.all
