8b4ab0b1d6ada81e3192114a9d41dadc9c006028
Katie Fricks <katiefricks@gmail.com>
2018-10-17 09:45:14 -0400

    Added payments.js via stripe documentation

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,10 @@
 class PurchaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_book, except: [:index]
+
   def index
-    # ...
+    @sales = Sale.where(user: current_user)
+    @sales = current_user.sales
   end
 
   def show
@@ -8,11 +12,11 @@ class PurchaseController < ApplicationController
   end
 
   def new
-    # ...
+    @purchase = Purchase.new
   end
 
   def create
-    # ...
+    @purchase = Purchasce.new(book: @book, user: current_user)
   end
 
   def edit
@@ -23,7 +27,10 @@ class PurchaseController < ApplicationController
     # ...
   end
 
-  def destroy
-    # ...
-  end
+
+private
+
+  def load_book
+    @book = Book.find(params[:book_id])
+
 end
