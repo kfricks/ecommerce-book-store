85d18d7e35fefb013be4187865c16ee1618062c2
Katie Fricks <katiefricks@gmail.com>
2018-10-18 15:00:14 -0400

    Added images to seed content

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@
 
 #puts everything in transaction container so i don't have to recreate users each time
 ActiveRecord::Base.transaction do
-  User.create!(name: "Katie", email: "user@user.com", password:"tester")
+  User.create!(name: "Katie", email: "katief@katie.com", password:"tester")
 
-  kurt_v = User.create!(name: "Kurt Vonny", email: "author@author.com", password:"tester")
+  kurt_v = User.create!(name: "Kurt Vonny", email: "kurtv@kurt.com", password:"tester")
 
   books = [
-    { title: "Book1", price: 15.99 },
-    { title: "Book2", price: 24.99 }
+    { title: "Book1", price: 15.99, author: kurt_v, image: "/images/Enchantment-Book-Cover-Best-Seller1.jpg" },
+    { title: "Book2", price: 24.99, author: kurt_v, image: "/images/Enchantment-Book-Cover-Best-Seller1.jpg"}
     ]
   books.each do |book|
-      Book.create!(title: book[:title], price: book[:price])
+      Book.create!(title: book[:title], price: book[:price], author: book[:author], avatar: book[:image])
   end
 
   kurt_v.books << Book.all
