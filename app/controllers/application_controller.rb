ff6d3cd9b43dc3834b1f9fe1af95124d907f9cd3
Katie Fricks <katiefricks@gmail.com>
2018-10-20 20:37:39 -0400

    Added 'name' input field to sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+   # added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :name]
+   # devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+   # devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
