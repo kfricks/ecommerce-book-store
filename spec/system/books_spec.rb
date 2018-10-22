c380e5b3ac241b0a4543d6a209c5a1ec24ca0242
Katie Fricks <katiefricks@gmail.com>
2018-10-22 16:29:57 -0400

    rspec no longer displays browser window

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Books", type: :system do
-  # before do
-  #   driven_by(:rack_test)
-  # end
+  before do
+    driven_by(:rack_test)
+  end
   it "has an index page of all books" do
     first_book = create(:book)
     second_book = create(:book)
